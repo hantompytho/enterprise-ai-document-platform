91a536b05942cf6bce721f19d6d1642de2967b76
Alex Petro <alexpetro020917@gmail.com>
2026-07-19 19:51:16 +0200

    feat: handle duplicate user registration

--- src/main/java/com/alexpetro/eadp/config/SecurityConfig.java
+++ src/main/java/com/alexpetro/eadp/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                                 "/api/health",
                                 "/api/auth/register",
                                 "/swagger-ui/**",
+                                "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
